9928989464a076e8c9fde05d98e7bab06092573a
ichabodcole <ichabodcole@gmail.com>
2013-08-12 23:12:47 -0700

    Add api controllers and models. Add basic Backbone directories to js client folder

---
+++ api/models/Messages.js
@@ -0,0 +1,17 @@
+/**
+ * Messages
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ *
+ */
+
+module.exports = {
+
+  attributes: {
+    userId: 'INT',
+    username: 'STRING',
+    message: 'STRING'
+  }
+
+};
---
+++ api/models/Users.js
@@ -0,0 +1,19 @@
+/**
+ * Users
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ *
+ */
+
+module.exports = {
+
+  attributes: {
+  	
+  	/* e.g.
+  	nickname: 'string'
+  	*/
+    
+  }
+
+};

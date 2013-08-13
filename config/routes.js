cbb1fbfb13899407b44a1668959d194dfc4d71b1
ichabodcole <ichabodcole@gmail.com>
2013-08-12 23:10:34 -0700

    Add MainController routes

--- config/routes.js
+++ config/routes.js
@@ -30,11 +30,20 @@ module.exports.routes = {
 
   // By default, your root route (aka home page) points to a view
   // located at `views/home/index.ejs`
-  // 
+  //
   // (This would also work if you had a file at: `/views/home.ejs`)
-  '/': {
-    view: 'home/index'
-  }
+
+  // '/': {
+  //   view: 'home/index'
+  // },
+
+  '/': 'MainController',
+
+  '/signup': 'MainController.signup',
+
+  '/login': 'MainController.login',
+
+  '/chat': 'MainController.chat',
 
   /*
   // But what if you want your home page to display
@@ -68,7 +77,7 @@ module.exports.routes = {
   // If you want to set up a route only for one in particular
   // (GET, POST, PUT, DELETE, etc.), just specify the verb before the path.
   // For example, if you have a `UserController` with a `signup` action,
-  // and somewhere else, you're serving a signup form looks like: 
+  // and somewhere else, you're serving a signup form looks like:
   //
   //		<form action="/signup">
   //			<input name="username" type="text"/>
@@ -166,4 +175,3 @@ module.exports.routes = {
  * Finally, if nothing else matched, the default 404 handler is triggered.
  * See `config/404.js` to adjust your app's 404 logic.
  */
- 

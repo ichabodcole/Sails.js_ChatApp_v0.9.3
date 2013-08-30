f9b42c29c145eac21708447c49b0006ee606d36b
ichabodcole <ichabodcole@gmail.com>
2013-08-29 17:41:53 -0700

    Minor organization updates, add comments

--- assets/js/app/controllers/message_list_controller.js
+++ assets/js/app/controllers/message_list_controller.js
@@ -4,10 +4,7 @@
   chatApp.controller('MessageListController', function($scope, socket, socketHttp, messagesResource, messageValidator, errorHandler) {
 
     var updateMessages = function updateMessages() {
-      socketHttp.get('/messages', function(res){
-        $scope.messages = res;
-      });
-      // messagesResource.query();
+      $scope.messages = messagesResource.query();
     };
 
     $scope.destroy = function (obj) {
@@ -31,13 +28,17 @@
       errorHandler.showErrors();
     };
 
-    // Load in the messages when the socket has connected.
-    socket.on('connect', function(){
-      updateMessages();
-    })
-
+    // Update the list of messages anytime a message
+    // is emitted from the server.
     socket.on('message', function(message){
       updateMessages();
+    });
+
+    // Load in the messages.
+    // Need to fetch the first set with socket to
+    // subscribe to any events sent to the messages resource.
+    socketHttp.get('/messages', function(res){
+      $scope.messages = res;
     });
   });
 }());

ac29944b502fa1c78874d43a3b15d8dfa213abe4
ichabodcole <ichabodcole@gmail.com>
2013-09-09 21:24:19 -0700

    Update MessageListController to directly manipulate the messages array vs querying on every message emitted by socket.io. The response message is now parsed and the appropriate actions are applied to the message array

--- assets/js/app/controllers/message_list_controller.js
+++ assets/js/app/controllers/message_list_controller.js
@@ -1,11 +1,65 @@
 (function() {
   'use strict';
 
-  chatApp.controller('MessageListController', function($scope, socket, socketHttp, messagesResource, messageValidator, errorHandler) {
+  chatApp.controller('MessageListController', function($scope, $filter, socket, socketHttp, messagesResource, messageValidator, errorHandler) {
 
-    var updateMessages = function updateMessages() {
-      $scope.messages = messagesResource.query();
-    };
+    var createMessage = function (resObj){
+      var messageObj = resObj.data;
+      $scope.messages.push(messageObj);
+    }
+
+    var updateMessage = function (resObj){
+      var messageObj = resObj.data;
+      console.log(messageObj);
+    }
+
+    var destroyMessage = function (resObj){
+      var messageId = resObj.id;
+      var messageIndex = getIndexById($scope.messages, messageId);
+      $scope.messages.splice(messageIndex, 1);
+    }
+
+    var getIndexById = function(array, id){
+      return getIndexByAttr(array, 'id', id);
+    }
+
+    var getIndexByAttr = function(array, attr, value){
+      var itemIndex = void 0;
+      array.forEach(function(element, index, array){
+        if(element[attr] === value){
+          itemIndex = index;
+        }
+      });
+      return itemIndex;
+    }
+
+    // create an actions object that points our http verbs
+    // to functions.
+    var actions = {
+      create: createMessage,
+      update: updateMessage,
+      destroy: destroyMessage,
+      defaultAction: function(message){
+        console.log('no action found', message);
+      }
+    }
+
+    // Update the list of messages anytime a message
+    // is emitted from the server.
+    socket.on('message', function(message){
+      var verb = message.verb;
+
+      /**
+      * Check to see if a corrosponding http verb exists in our
+      * actions object, if it does execute the function
+      * and if not trigger the default action.
+      **/
+      if(actions[verb]){
+        actions[verb](message);
+      }else{
+        actions.defaultAction(message);
+      }
+    });
 
     $scope.destroy = function (obj) {
       var messageId = obj.messageId;
@@ -27,12 +81,6 @@
 
       errorHandler.showErrors();
     };
-
-    // Update the list of messages anytime a message
-    // is emitted from the server.
-    socket.on('message', function(message){
-      updateMessages();
-    });
 
     // Load in the messages.
     // Need to fetch the first set with socket to
